7d78834cb1b85ab1c3852fbf2b53d4f9a4a34b2c
Can Gokhan Kiziloglu <membercgk@me.com>
2023-11-29 12:41:14 +0300

    booking and menu models created and migrated

--- Restaurant/admin.py
+++ Restaurant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Booking, Menu
 
 # Register your models here.
+admin.site.site_header = "LittleLemon Restaurant"
+admin.site.site_title = "LittleLemon Restaurant Admin Portal"
+admin.site.index_title = "Welcome to LittleLemon Restaurant Portal"
+
+admin.site.register(Booking)
+admin.site.register(Menu)
