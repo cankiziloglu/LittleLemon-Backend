a36d3d206ef53864980ff863a0de21451ef76739
Can Gokhan Kiziloglu <membercgk@me.com>
2023-11-29 12:59:10 +0300

    DRF installed, serializers, views and urls updated

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from .views import *
 
+
+router = routers.DefaultRouter()
+router.register("booking", BookingViewSet)
+router.register("menu", MenuViewSet)
+
 urlpatterns = [
-    path('', index, name='index'),
+    path("", index, name="index"),
+    path("api/", include(router.urls)),
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+     queryset = Booking.objects.all()
+     serializer_class = BookingSerializer
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+     queryset = Menu.objects.all()
+     serializer_class = MenuSerializer
+
