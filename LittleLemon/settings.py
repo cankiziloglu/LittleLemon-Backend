59a110230ebbe8dfc67262c962108a1cd0c7248a
Can Gokhan Kiziloglu <membercgk@me.com>
2023-11-29 15:55:08 +0300

    djoser installed, token auth implemented

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "Restaurant",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+}
